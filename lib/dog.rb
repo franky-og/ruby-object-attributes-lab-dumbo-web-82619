a8a3620c3645ba589be85bac5190e6e12cd6c646
Franky Liu <cuifranky95@gmail.com>
2019-08-28 13:18:34 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -21,3 +21,27 @@ def breed=(new_breed)
   @breed= new_breed
 end
 end
+
+class Person
+  def initialize
+    @name = name
+  end
+  def name
+    @name
+  end
+  def name=(new_name)
+    @name= new_name
+  end
+end
+
+class Person
+  def initialize
+    @job = job
+  end
+  def job
+    @job
+  end
+  def job= (new_job)
+    @job= new_job
+  end
+end
